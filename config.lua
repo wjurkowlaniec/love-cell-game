bc177d49f3d900a4559ae95ef01aa63f1ab4090d
wjurkowl <wjurkowl@cisco.com>
2023-03-14 19:16:22 +0100

    fixes for PR

--- config.lua
+++ config.lua
@@ -8,7 +8,7 @@ FRAME_TIME = 1 / 60
 WINDOW_WIDTH = 900
 WINDOW_HEIGHT = 700
 
-CELL_IMG_WIDTH, CELL_IMG_HEIGHT = 150, 150
+CELL_IMG_WIDTH, CELL_IMG_HEIGHT = 200, 200
 
 CELL_RADIUS = CELL_IMG_WIDTH / 2
 
@@ -22,11 +22,17 @@ BoundaryTop = CELL_SPAWN_BORDER_Y + CELL_IMG_HEIGHT / 2
 BoundaryBottom = WINDOW_HEIGHT - CELL_SPAWN_BORDER_Y - CELL_IMG_HEIGHT / 2
 
 BgColor = {}
+
+
+
+
+
+
 BgColor.R, BgColor.G, BgColor.B = getColorFromHex("#3e4453")
 
 TextInsideCellColor = table.pack(getColorFromHex("#000000"))
 
-DefaultColor = table.pack(getColorFromHex("#FFFFFF"))
+DefaultColor = table.pack(getColorFromHex("#FFFFFD"))
 
 Enemy1LineColor = table.pack(getColorFromHex("#ff0004"))
 PlayerLineColor = table.pack(getColorFromHex("#00fb00"))
